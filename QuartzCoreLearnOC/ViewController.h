f625f4bad2678354ff0a30e1756a1bf19c5cee9c
陶冬波 <785429438@qq.com>
2018-06-19 14:44:42 +0800

    Initial Commit

---
+++ QuartzCoreLearnOC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QuartzCoreLearnOC
+//
+//  Created by 陶冬波 on 2018/6/19.
+//  Copyright © 2018年 陶冬波. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QuartzCoreLearnOC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QuartzCoreLearnOC
+//
+//  Created by 陶冬波 on 2018/6/19.
+//  Copyright © 2018年 陶冬波. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
